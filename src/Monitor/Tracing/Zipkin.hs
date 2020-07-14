0353f8de33f2463f584ed0104cd20ba77eef9a97
Alejandro Serrano <trupill@gmail.com>
2020-07-14 11:19:03 +0200

    Fix from https://github.com/mtth/tracing/pull/1

--- src/Monitor/Tracing/Zipkin.hs
+++ src/Monitor/Tracing/Zipkin.hs
@@ -132,6 +132,7 @@ new (Settings mbHostname mbPort mbEpt mbMgr mbPrd) = liftIO $ do
     req = HTTP.defaultRequest
       { HTTP.method = "POST"
       , HTTP.host = BS.pack (fromMaybe "localhost" mbHostname)
+      , HTTP.requestHeaders = [("Content-Type", "application/json")]
       , HTTP.path = "/api/v2/spans"
       , HTTP.port = maybe 9411 fromIntegral mbPort
       , HTTP.requestHeaders = [("content-type", "application/json")]

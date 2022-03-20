5b9e5c93266b97bb3cd95cc2b6a54eb578444d9b
Alejandro Serrano <trupill@gmail.com>
2022-03-20 19:35:20 +0100

    Updates for 9.2

--- src/Control/Monad/Trace.hs
+++ src/Control/Monad/Trace.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ImpredicativeTypes #-}
 {-# LANGUAGE InstanceSigs #-}
